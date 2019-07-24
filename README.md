9048210899de0f1008f1a3cb9a3672859d965b8b
alf808 <alf@akademe.net>
2019-07-24 11:50:39 -0700

    fixed create_ip_list

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# peeweesweep-kale
+# PeeweeSweep
+PySweep is a command line utility based on utilizing some basic core Python functionality and the `fping` utility of the operating system. 
--- pysweep.py
+++ pysweep.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-'''Pysweep'''
+'''ping sweep utility to automate and capture some of the results of
+a ping sweep across the network
+
+Usage:
+        ./pysweep.py <IP> <CIDR>
+'''
 import subprocess
+import sys
 
 # global variables
 ip_list = []
-cidr_tab = {24: 255}
+cidr_table = {24: 255}
 
 
-def create_ip_list(ip_target, cidr):
+def create_ip_list(ip, cidr):
     '''create a list of IP addresses'''
-    last_octet = range(cidr_tab[cidr])
-    ip_target_list = [ip_target + str(x) for x in last_octet]
+    cidr_int = int(cidr.strip('/'))
+    last_octet_list = get_cidr_range(cidr_int)
+    ip_target = get_three_octets(ip)
+    ip_target_list = [ip_target + '.' + str(x) for x in last_octet_list]
     return ip_target_list
     
 
@@ -30,12 +38,23 @@ def fping_ip(ip):
     ip_list.append(output)
 
 
-def main():
-    ip_target = "192.168.200."
-    ip_target_list = create_ip_list(ip_target, 24)
+def get_three_octets(ip):
+    ip_temp = ip.split('.')
+    ip_temp.pop()
+    return '.'.join(ip_temp)
+
+
+def get_cidr_range(cidr):
+    # TODO: fix for cidr 27
+    return range(cidr_table[cidr])
+    
+
+def main(ip, cidr):
+    ip_target_list = create_ip_list(ip, cidr)
     for ip in ip_target_list:
         fping_ip(ip)
     
 
 if __name__ == "__main__":
-    main()
+        main("192.168.0.108", "/24")
+#    main(sys.argv[1], sys.argv[2])
